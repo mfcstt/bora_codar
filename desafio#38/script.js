2358826c05ee9551e58418f398dadda7a63fc1cb
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2023-09-21 23:58:42 +0000

    fisrt

--- desafio#38/script.js
+++ desafio#38/script.js
@@ -1,85 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const submitButton = document.getElementById("submit-button");
+    const submitButton = document.getElementById("finishButton");
     const scoreDisplay = document.getElementById("score");
+    const questionDisplay = document.getElementById("question");
+    const answerInputs = document.querySelectorAll('input[type="radio"]');
 
-    // Defina aqui as perguntas e respostas corretas
     const questions = [
         {
             question: "Qual era o objetivo da personagem?",
             correctAnswer: "c"
         },
-       
+        
     ];
 
+    
+
+    let currentQuestionIndex = 0;
+    let score = 0;
+
+    function loadQuestion(index) {
+        if (index < questions.length) {
+            const currentQuestion = questions[index];
+            questionDisplay.textContent = currentQuestion.question;
+
+            answerInputs.forEach((input) => {
+                input.checked = false;
+            });
+        } else {
+            questionDisplay.textContent = "Quiz Concluído!";
+            submitButton.style.display = "none";
+        }
+    }
+
+    loadQuestion(currentQuestionIndex);
+
     submitButton.addEventListener("click", () => {
-        let score = 0;
+        const selectedAnswer = document.querySelector('input[type="radio"]:checked');
 
-        // Verifique as respostas
-        questions.forEach((question, index) => {
-            const selectedAnswer = document.querySelector(`input[name="pergunta${index + 1}"]:checked`);
-
-            if (selectedAnswer) {
-                if (selectedAnswer.value === question.correctAnswer) {
-                    score++;
-                }
+        if (selectedAnswer) {
+            if (selectedAnswer.value === questions[currentQuestionIndex].correctAnswer) {
+                score++;
             }
-        });
 
-        // Exiba o score
-        scoreDisplay.textContent = score + " / " + questions.length;
-    });
-});
-
-
-
-
-let cardSelected = false;
-let card1Selected = false;
-let card2Selected = false;
-
-
-// Adicione um evento de clique a cada card
-const cards = document.querySelectorAll(".card, .card1, .card2");
-
-cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        if (!cardSelected) {
-            card.style.backgroundColor = "#1d2330"; 
-            cardSelected = true;
-            cards.forEach((otherCard) => {
-                if (otherCard !== card) {
-                    otherCard.style.pointerEvents = "none";
-                }
-            });
+            currentQuestionIndex++;
+            loadQuestion(currentQuestionIndex);
+            scoreDisplay.textContent = score + " / " + questions.length;
         }
     });
 });
 
-cards.forEach((card1) => {
-    card1.addEventListener("click", () => {
-        if (!card1Selected) {
-            card1.style.backgroundColor = "#1d2330"; 
-            card1Selected = true;
-            cards.forEach((otherCard) => {
-                if (otherCard !== card1) {
-                    otherCard.style.pointerEvents = "none";
-                }
-            });
-        }
-    });
-});
 
-cards.forEach((card2) => {
-    card2.addEventListener("click", () => {
-        if (!card2Selected) {
-            card2.style.backgroundColor = "#1d2330"; 
-            card2Selected = true;
-            cards.forEach((otherCard) => {
-                if (otherCard !== card2) {
-                    otherCard.style.pointerEvents = "none";
-                }
-            });
-        }
-    });
-});
+
 
