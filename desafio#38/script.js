8bc9caa318e73ed97705fc6a447033eef78f0bba
aluno <aluno@ftcbp.edu.br>
2023-09-21 11:11:48 -0300

    event click

--- desafio#38/script.js
+++ desafio#38/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Defina aqui as perguntas e respostas corretas
     const questions = [
         {
-            question: "O que Lila ganhou do gnomo?",
+            question: "Qual era o objetivo da personagem?",
             correctAnswer: "c"
         },
        
@@ -29,3 +29,57 @@ document.addEventListener("DOMContentLoaded", function () {
         scoreDisplay.textContent = score + " / " + questions.length;
     });
 });
+
+
+
+
+let cardSelected = false;
+let card1Selected = false;
+let card2Selected = false;
+
+
+// Adicione um evento de clique a cada card
+const cards = document.querySelectorAll(".card, .card1, .card2");
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        if (!cardSelected) {
+            card.style.backgroundColor = "#1d2330"; 
+            cardSelected = true;
+            cards.forEach((otherCard) => {
+                if (otherCard !== card) {
+                    otherCard.style.pointerEvents = "none";
+                }
+            });
+        }
+    });
+});
+
+cards.forEach((card1) => {
+    card1.addEventListener("click", () => {
+        if (!card1Selected) {
+            card1.style.backgroundColor = "#1d2330"; 
+            card1Selected = true;
+            cards.forEach((otherCard) => {
+                if (otherCard !== card1) {
+                    otherCard.style.pointerEvents = "none";
+                }
+            });
+        }
+    });
+});
+
+cards.forEach((card2) => {
+    card2.addEventListener("click", () => {
+        if (!card2Selected) {
+            card2.style.backgroundColor = "#1d2330"; 
+            card2Selected = true;
+            cards.forEach((otherCard) => {
+                if (otherCard !== card2) {
+                    otherCard.style.pointerEvents = "none";
+                }
+            });
+        }
+    });
+});
+
