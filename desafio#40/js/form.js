dabcaa6c8becb3cb9b741f1acf116719c755de60
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2023-10-05 20:52:03 +0000

    inserir funcionalidade

---
+++ desafio#40/js/form.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecione o formulário e o botão pelo ID
+    const form = document.getElementById('newsletter-form');
+    const button = document.getElementById('button');
+
+    // Adicione um evento de clique ao botão
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede que o formulário seja enviado (caso ele esteja dentro de um formulário maior)
+
+        // Limpe o campo de e-mail e reinicie a página
+        const emailInput = document.getElementById('email');
+        emailInput.value = '';
+        location.reload();
+    });
+});
